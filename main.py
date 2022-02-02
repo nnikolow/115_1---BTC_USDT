96df20fd5dcb7145836ecce255d3fc741d8bf502
Nikolay.Nikolov2 <nikolay.nikolov2@adastragrp.com>
2022-02-02 15:03:00 +0200

    c1

--- main.py
+++ main.py
@@ -1,17 +1,37 @@
 import websocket
+import math
 import json
 from datetime import datetime
+import time
+import threading
+
+
 
 
 def sort_message(message):
     for data in json.loads(message)['data']:
         print(datetime.utcfromtimestamp(data['t'] / 1000).strftime('%Y-%m-%d %H:%M:%S'),
               "price:", data['p'],
-              "volume:", data['v']),
+              "volume:", data['v'])
+
+
+
+def one_minute(message):
+    sums = 0
+    # long = json.loads(message['c'])
+    # print(long)
+    # print(message['c'])
+    for data in json.loads(message)['data']:
+        sums += data['p']
+    totalsums = float(sums)
+    print(round(totalsums, 2))
 
 
 def on_message(ws, message):
     sort_message(message)
+    one_minute(message)
+    # timer = threading.Timer(10.0, one_minute(message))
+    # timer.start()
 
 
 def on_error(ws, error):
